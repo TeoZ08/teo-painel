f6921c1cd2450182cc7a751e40aeaab96c574bb2
Matteo <matteoscotti10@gmail.com>
2026-07-19 14:14:25 -0400

    fix: usar askpass para fonte privada

--- services/context-materializer/start.sh
+++ services/context-materializer/start.sh
@@ -2,9 +2,17 @@
 set -eu
 
 if [ -n "${CONTEXT_SOURCE_READ_TOKEN:-}" ]; then
-  export GIT_CONFIG_COUNT=1
-  export GIT_CONFIG_KEY_0='url.https://x-access-token:'"${CONTEXT_SOURCE_READ_TOKEN}"'@github.com/.insteadOf'
-  export GIT_CONFIG_VALUE_0='https://github.com/'
+  cat > /tmp/git-askpass <<'EOF'
+#!/bin/sh
+case "$1" in
+  *Username*) printf '%s\n' 'x-access-token' ;;
+  *Password*) printf '%s\n' "$CONTEXT_SOURCE_READ_TOKEN" ;;
+  *) exit 1 ;;
+esac
+EOF
+  chmod 700 /tmp/git-askpass
+  export GIT_ASKPASS=/tmp/git-askpass
+  export GIT_TERMINAL_PROMPT=0
 fi
 
 exec node services/context-materializer/server.mjs
